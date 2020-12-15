c02d49d0e8f3f3a985f731055024fde33694ac88
b.saintot <bsaintot@octo.com>
2020-12-15 15:03:18 +0100

    Add README.md and setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+# Airflow vs. Prefect
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='airflow-prefect',
+    version='1.0.0',
+    url='https://gitlab.com/bsaintot/airlflow-prefect',
+    author='BASA',
+    author_email='basa@octo.com',
+    description='An illustrated comparison of Prefect and Airflow 2.0',
+    packages=find_packages(),
+    install_requires=['pandas >= 1.1.3',
+                      'prefect == 0.13.19',
+                      'scikit-learn >= 0.23.2'
+                      ],
+)
